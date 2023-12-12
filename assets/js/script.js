691316c0f804b115cb5a7740f4e48af170b0b697
akent <alexanderkent099@gmail.com>
2023-12-12 15:44:18 -0500

    Add quiz questions with answers for JS and HTML topics

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,77 @@ var initialsForm = document.getElementById('score-form');
 var highScoresScreen = document.getElementById('high-scores');
 var scoreList = document.getElementById("go-back");
 var clearScoresButton = document.getElementById('clear-scores');
+
+var currentQuestionIndex, questions, timer, score;
+
+questions = [
+    { question: "In HTML, which of the following is the correct way for showing how to import an external script?", answers: [
+        "<script add='index.js'></script>",
+        "<script src='index.js'></script>",
+        "<script adding-js='index.js'></script>",
+        "<script js='index.js'></script>"
+    ], correct: 0 },
+
+    { question: "How is the document type initialized in HTML5?", answers: [
+        "</DOCTYPE HTML>",
+        "</DOCTYPE>",
+        "<!HTML>",
+        "<!DOCTYPE HTML>"
+    ], correct: 3 },
+
+    { question: "In JavaScript, what is the name of the method used to remove white space from the beginning and end of a string?", answers: [
+        ".reduce()",
+        ".trim()",
+        ".slice()",
+        ".substring()"
+    ], correct: 1 }, 
+
+    { question: "In JavaScript, a variable that has been declared but not assigned a value is known as...?", answers: [
+        "unknown",
+        "pending",
+        "undefined",
+        "unassigned"
+    ], correct: 2 },
+
+    { question: "In JavaScript, which comparison operator is used to express strict equality?", answers: [
+        ">=",
+        "+=",
+        "<=",
+        "==="
+    ], correct: 3 },
+
+    { question: "What does JSON stand for?", answers: [
+        "Jupiter Script Object Notation",
+        "JavaScript Object Notation",
+        "Jabber Script Object Notation",
+        "Jargon Script Object Notation"
+    ], correct: 1 },
+
+    { question: "In JavaScript, what is the logical AND operator?", answers: [
+        "||",
+        "**",
+        "!",
+        "&&"
+    ], correct: 3 },
+
+    { question: "In JavaScript, what is the name of the method used to add new elements to the DOM tree?", answers: [
+        "Callback function",
+        ".querySelectorAll()",
+        "document.createElement()",
+        ".textContent()"
+    ], correct: 2 },
+
+    { question: "In JavaScript, what type of value is an Array?", answers: [
+        "Object",
+        "Array",
+        "Map",
+        "Set",
+    ], correct: 0 },
+
+    { question: "In JavaScript, what is it called when an event is triggered on a target element and subsequent parent elements?", answers: [
+        "Event propagation",
+        "Event bubbling",
+        "Event capturing",
+        "Event handling",
+    ], correct: 1 },
+];
